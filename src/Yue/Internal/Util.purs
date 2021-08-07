9406f0fe83ed1ed09aa049eb49fc9d39edbf976d
荀徒之 <kalxd@126.com>
2021-08-07 13:48:35 +0800

    添加splitString。

---
+++ src/Yue/Internal/Util.purs
@@ -0,0 +1,18 @@
+-- | 内部函数。
+
+module Yue.Internal.Util where
+
+import Prelude
+
+import Data.Array (mapMaybe)
+import Data.String (Pattern(..), split, trim)
+import Data.String.NonEmpty (NonEmptyString, fromString)
+
+-- | 把一条String，根据"/"分割成多条非空String，自动过滤掉空组。
+-- |
+-- | ```
+-- | splitString "a/c" = ["a", "c"]
+-- | splitString "/a//c" = ["a", "c"]
+-- | ```
+splitString :: String -> Array NonEmptyString
+splitString = mapMaybe fromString <<< split (Pattern "/") <<< trim
