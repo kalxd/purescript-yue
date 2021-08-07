79c01a2d071481fce075e288de7078502cedd587
荀徒之 <kalxd@126.com>
2021-08-07 10:18:18 +0800

    删除tryParseParam，(Maybe a)实现为IsParamParsable一个实例。

--- src/Yue/Parsable.purs
+++ src/Yue/Parsable.purs
@@ -1,12 +1,11 @@
-module Yue.Parsable ( tryParseParam
-                    , parseParam
-                    , class IsParamParsable) where
+module Yue.Parsable ( class IsParamParsable
+                    , parseParam) where
 
 import Prelude
 
-import Data.Either (Either(..), hush, note)
+import Data.Either (Either(..), note)
 import Data.Int as IInt
-import Data.Maybe (Maybe)
+import Data.Maybe (Maybe(..))
 import Data.Number as INumber
 
 -- | 将一小截param参数，转化成必要的类型。
@@ -24,5 +23,7 @@ instance IsParamParsable Int where
   parseParam s = note msg $ IInt.fromString s
     where msg = s <> "无法转化为Int"
 
-tryParseParam :: forall a. IsParamParsable a => String -> Maybe a
-tryParseParam = hush <<< parseParam
+instance IsParamParsable a => IsParamParsable (Maybe a) where
+  parseParam s = case parseParam s of
+    Right a -> Right a
+    Left _ -> Right Nothing
