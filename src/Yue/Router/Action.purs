4679f3c03eddd5ddf8abaf0a11d2db799eb3f7b4
荀徒之 <kalxd@126.com>
2021-08-11 15:11:57 +0800

    我好像知道该怎么写了。

--- src/Yue/Router/Action.purs
+++ src/Yue/Router/Action.purs
@@ -5,26 +5,25 @@ module Yue.Router.Action ( ActionT
 
 import Prelude
 
-import Control.Monad.Reader.Trans (class MonadAsk, ReaderT, asks)
+import Control.Monad.Reader.Trans (ReaderT, asks)
 import Control.Monad.State.Trans (StateT)
 import Data.Array (find)
 import Data.Maybe (Maybe)
 import Data.Tuple (fst, snd)
-import Effect.Class (class MonadEffect)
 import Node.HTTP as HTTP
 import Node.Stream as Stream
 import URI.Extra.QueryPairs as Q
 
 type ActionST = { req :: HTTP.Request
                 , res :: HTTP.Response
-                , output :: Stream.Writable ()
+                , outputSteam :: Stream.Writable ()
                 , query :: Q.QueryPairs Q.Key Q.Value
                 }
 
 -- | 单条请求的状态。
 type ActionT s m a = ReaderT ActionST (StateT s m) a
 
-query :: forall s m. MonadAsk ActionST m => MonadEffect m => String -> ActionT s m (Maybe String)
+query :: forall s m. Monad m => String -> ActionT s m (Maybe String)
 query key = do
   (Q.QueryPairs qs) <- asks _.query
   let f x = Q.keyFromString key == fst x
