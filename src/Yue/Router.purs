9a889531ef75fd301a7633e558b4173d7846c7e2
荀徒之 <kalxd@126.com>
2021-08-12 10:46:37 +0800

    添加几种常用方法。

--- src/Yue/Router.purs
+++ src/Yue/Router.purs
@@ -1,12 +1,17 @@
 module Yue.Router ( RouterMap
                   , getRouter
+                  , get
+                  , post
+                  , put
+                  , delete
                   ) where
 
 import Prelude
 
 import Data.Array (findMap)
 import Data.Maybe (Maybe(..))
-import Yue.Router.Path (RouterPath, stripPathPrefix)
+import Yue.Router.Action (ActionT)
+import Yue.Router.Path (RouterPath, mapToRouterPath, stripPathPrefix)
 
 type RouterKey = { path :: RouterPath
                  , method :: String
@@ -24,3 +29,22 @@ getRouter key (RouterNode path xs) = do
   findMap (getRouter key') xs
 getRouter key (RouterEnd k a) | key == k = Just a
                               | otherwise = Nothing
+
+route' :: forall s m a. String -> String -> ActionT s m a -> RouterMap (ActionT s m a)
+route' method path = RouterEnd key
+  where path' = mapToRouterPath path
+        key = { path: path', method }
+
+type RouterApp = forall s m a. String -> ActionT s m a -> RouterMap (ActionT s m a)
+
+get :: RouterApp
+get = route' "GET"
+
+post :: RouterApp
+post = route' "POST"
+
+put :: RouterApp
+put = route' "PUT"
+
+delete :: RouterApp
+delete = route' "DELETE"
