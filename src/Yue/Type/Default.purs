6b8ad9a6a3af15d38acc2e6b53a961e058a61d88
荀徒之 <kalxd@126.com>
2021-08-23 14:24:46 +0800

    添加Default。

---
+++ src/Yue/Type/Default.purs
@@ -0,0 +1,21 @@
+module Yue.Type.Default where
+
+import Data.Maybe (Maybe(..))
+
+class Default a where
+  def :: a
+
+instance Default String where
+  def = ""
+
+instance Default Int where
+  def = 0
+
+instance Default Number where
+  def = 0.0
+
+instance Default (Array a) where
+  def = []
+
+instance Default a => Default (Maybe a) where
+  def = Just def
