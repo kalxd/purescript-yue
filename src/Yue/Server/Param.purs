af30e017e6596f438a72d1448740cb481b60916d
荀徒之 <kalxd@126.com>
2021-08-23 14:30:21 +0800

    param'

--- src/Yue/Server/Param.purs
+++ src/Yue/Server/Param.purs
@@ -1,5 +1,6 @@
 module Yue.Server.Param ( tryParam
                         , param
+                        , param'
                         ) where
 
 import Prelude
@@ -7,7 +8,7 @@ import Prelude
 import Control.Monad.State.Trans (get)
 import Data.HashMap as Map
 import Data.Maybe (Maybe(..))
-import Yue.Internal.Type.Action (ActionT, catchAction, exceptEither, throwChecked)
+import Yue.Internal.Type.Action (ActionT, catchAction, exceptEither, fromMaybeAction, throwChecked)
 import Yue.Internal.Type.MatchState (MatchState(..))
 import Yue.Internal.Type.Parsable (class Parsable, parseParam)
 
@@ -20,3 +21,6 @@ param key = do
 
 tryParam :: forall e m a. Parsable a => Monad m => String -> ActionT e m (Maybe a)
 tryParam = catchAction <<< param
+
+param' :: forall e m a. Parsable a => Monad m => a -> String -> ActionT e m a
+param' a p = fromMaybeAction (tryParam p) a
