68a459e715c5fd91c548a6e68ec05256e5afa516
荀徒之 <kalxd@126.com>
2021-10-03 10:10:08 +0800

    详细解释param失败原因。

--- src/Yue/Server/Param.purs
+++ src/Yue/Server/Param.purs
@@ -13,6 +13,7 @@ import Data.Maybe (Maybe(..))
 import Yue.Internal.Type.Action (ActionT, fromMaybeAction)
 import Yue.Internal.Type.MatchState (MatchState(..))
 import Yue.Internal.Type.Parsable (class Parsable, parseParam)
+import Yue.Internal.Util (mapLeft)
 
 data ParamError = ParamNotFound String
                 | ParamParseFail String
@@ -23,14 +24,18 @@ instance Show ParamError where
   show (ParamNotFound msg) = show msg
   show (ParamParseFail msg) = show msg
 
+paramNotFound :: forall a. String -> Either ParamError a
+paramNotFound key = Left $ ParamNotFound $ "param不存在" <> key <> "字段！"
+
+fmtParamParseFail :: String -> String -> ParamError
+fmtParamParseFail key msg = ParamParseFail $ "param解析" <> key <> "字段失败：" <> msg <> "。"
+
 param :: forall e m a. Parsable a => Monad m => String -> ActionT e m (Either ParamError a)
 param key = do
   MatchState s <- get
   pure case Map.lookup key s.paramMap of
-    Nothing -> Left $ ParamNotFound $ "param参数不存在" <> key <> "字段！"
-    Just a -> case parseParam a of
-      Right a' -> Right a'
-      Left e -> Left $ ParamParseFail e
+    Nothing -> paramNotFound key
+    Just a -> mapLeft (fmtParamParseFail key) $ parseParam a
 
 tryParam :: forall e m a. Parsable a => Monad m => String -> ActionT e m (Maybe a)
 tryParam key = hush <$> param key
