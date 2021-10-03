63449ce87cf78f7bd1eb8e71c590f252a33d707d
荀徒之 <kalxd@126.com>
2021-10-03 09:09:32 +0800

    Param返回Either。

--- src/Yue/Server/Param.purs
+++ src/Yue/Server/Param.purs
@@ -1,26 +1,39 @@
 module Yue.Server.Param ( tryParam
                         , param
                         , param'
+                        , ParamError(..)
                         ) where
 
 import Prelude
 
 import Control.Monad.State.Trans (get)
+import Data.Either (Either(..), hush)
 import Data.HashMap as Map
 import Data.Maybe (Maybe(..))
-import Yue.Internal.Type.Action (ActionT, catchAction, exceptEither, fromMaybeAction, throwChecked)
+import Yue.Internal.Type.Action (ActionT, fromMaybeAction)
 import Yue.Internal.Type.MatchState (MatchState(..))
 import Yue.Internal.Type.Parsable (class Parsable, parseParam)
 
-param :: forall e m a. Parsable a => Monad m => String -> ActionT e m a
+data ParamError = ParamNotFound String
+                | ParamParseFail String
+
+derive instance Eq ParamError
+
+instance Show ParamError where
+  show (ParamNotFound msg) = show msg
+  show (ParamParseFail msg) = show msg
+
+param :: forall e m a. Parsable a => Monad m => String -> ActionT e m (Either ParamError a)
 param key = do
-  (MatchState s) <- get
-  case Map.lookup key s.paramMap of
-    Nothing -> throwChecked $ "param参数不存在" <> key
-    Just a -> exceptEither $ parseParam a
+  MatchState s <- get
+  pure case Map.lookup key s.paramMap of
+    Nothing -> Left $ ParamNotFound $ "param参数不存在" <> key <> "字段！"
+    Just a -> case parseParam a of
+      Right a' -> Right a'
+      Left e -> Left $ ParamParseFail e
 
 tryParam :: forall e m a. Parsable a => Monad m => String -> ActionT e m (Maybe a)
-tryParam = catchAction <<< param
+tryParam key = hush <$> param key
 
 param' :: forall e m a. Parsable a => Monad m => a -> String -> ActionT e m a
 param' a p = fromMaybeAction (tryParam p) a
