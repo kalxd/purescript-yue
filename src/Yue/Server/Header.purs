0744f9b6f757e622b786b61b3c723c642aa29e72
荀徒之 <kalxd@126.com>
2021-10-03 08:54:16 +0800

    header操作返回Either。

--- src/Yue/Server/Header.purs
+++ src/Yue/Server/Header.purs
@@ -4,27 +4,35 @@ module Yue.Server.Header ( tryHeader
                          , setHeader
                          , setJsonHeader
                          , setCode
+                         , HeaderError(..)
                          ) where
 
 import Prelude
 
 import Control.Monad.Reader.Trans (asks)
+import Data.Either (Either, note')
 import Data.Maybe (Maybe)
 import Data.String (toLower)
 import Effect.Class (class MonadEffect, liftEffect)
 import Foreign.Object (lookup)
 import Node.HTTP as HTTP
-import Yue.Internal.Type.Action (ActionT, fromMaybeAction, throwCheckedMaybe)
+import Yue.Internal.Type.Action (ActionT, fromMaybeAction)
 
-header :: forall e m. Monad m => String -> ActionT e m String
-header key = asks _.req >>= (throwCheckedMaybe msg <<< f)
-  where msg = "请求头并没有" <> key <> "！"
-        f = lookup (toLower key) <<< HTTP.requestHeaders
+newtype HeaderError = HeaderNotFound String
+
+derive instance Eq HeaderError
+
+instance Show HeaderError where
+  show (HeaderNotFound msg) = show msg
 
 tryHeader :: forall e m. Monad m => String -> ActionT e m (Maybe String)
 tryHeader key = do
   req <- asks _.req
   pure $ lookup (toLower key) $ HTTP.requestHeaders req
+
+header :: forall e m. Monad m => String -> ActionT e m (Either HeaderError String)
+header key = note' f <$> tryHeader key
+  where f _ = HeaderNotFound $ "请求头没有" <> key <> "字段！"
 
 header' :: forall e m. Monad m => String -> String -> ActionT e m String
 header' def key = fromMaybeAction (tryHeader key) def
